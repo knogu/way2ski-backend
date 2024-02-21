dbfcc50063f8278a1faf9d543d8d8353d6eb5a23
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2024-02-21 19:40:27 +0900

    Minimal grpc flame

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "way2ski-backend/proto"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedWayServiceServer
+}
+
+func (s *server) GetLines(ctx context.Context, in *pb.Params) (*pb.Lines, error) {
+	return &pb.Lines{
+		Lines: nil,
+	}, nil
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterWayServiceServer(s, &server{})
+	reflection.Register(s)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
